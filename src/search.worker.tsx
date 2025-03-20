3c82e9909084cee97e7f744be5027a33f0c2a2ed
Bubu <2005901+BubuMVX@users.noreply.github.com>
2025-03-21 00:57:50 +0100

    Small speed optimizations

--- src/search.worker.tsx
+++ src/search.worker.tsx
@@ -15,14 +15,13 @@ self.onmessage = async (event: MessageEvent) => {
     const searchContains = (event.data.searchContains ?? '').toLowerCase();
     const searchSuffix = (event.data.searchSuffix ?? '').toLowerCase();
     const maxIndex = (event.data.searchOnlyFirstIndex ? 1 : 1000);
-    console.log('searchOnlyFirstIndex', event.data.searchOnlyFirstIndex);
-    console.log('maxIndex', maxIndex);
 
     const withShard = searchShard > -1;
     const withPrefix = searchPrefix.length > 0;
     const withContains = searchContains.length > 0;
     const withSuffix = searchSuffix.length > 0;
 
+    const BATCH_SIZE = 100;
     let count = 0;
     let searching = true;
     const addressComputer = new AddressComputer();
@@ -32,25 +31,20 @@ self.onmessage = async (event: MessageEvent) => {
 
         for (let index = 0; index < maxIndex; index++) {
             count++;
-            postMessage({
-                'event': 'count',
-                'id': id,
-                'count': count,
-            });
+
+            if (count % BATCH_SIZE === 0) {
+                postMessage({
+                    'event': 'count',
+                    'id': id,
+                    'count': count,
+                });
+            }
 
             const key = mnemonic.deriveKey(index);
             const address = key.generatePublicKey().toAddress().bech32();
             const shortAddress = address.slice(4);
 
-            if (withShard && searchShard != addressComputer.getShardOfAddress(new Address(address))) {
-                continue;
-            }
-
             if (withPrefix && !shortAddress.startsWith(searchPrefix)) {
-                continue;
-            }
-
-            if (withContains && shortAddress.indexOf(searchContains) == -1) {
                 continue;
             }
 
@@ -58,16 +52,32 @@ self.onmessage = async (event: MessageEvent) => {
                 continue;
             }
 
-            const message = {
+            if (withContains && shortAddress.indexOf(searchContains) === -1) {
+                continue;
+            }
+
+            if (withShard) {
+                const shard = addressComputer.getShardOfAddress(new Address(address));
+
+                if (searchShard !== shard) {
+                    continue;
+                }
+            }
+
+            postMessage({
+                'event': 'count',
+                'id': id,
+                'count': count,
+            });
+            postMessage({
                 'event': 'success',
                 'id': id,
                 'address': address,
                 'mnemonic': mnemonic.toString(),
                 'shard': addressComputer.getShardOfAddress(new Address(address)),
                 'index': index,
-            };
-            console.log(`Worker #${id}`, message);
-            postMessage(message);
+            });
+
             searching = false;
             break;
         }
